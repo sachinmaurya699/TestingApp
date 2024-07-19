d9568fd7ec105998691796c694d44879e87b0294
Lenovo <sachinmaurya699@gmail.com>
2024-07-19 12:49:54 +0530

    by sachin commit

--- app/src/main/java/com/example/testingapp/Home_Activity.java
+++ app/src/main/java/com/example/testingapp/Home_Activity.java
@@ -17,7 +17,6 @@ public class Home_Activity extends AppCompatActivity
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
 
-
         //sachin...............
 
     }
